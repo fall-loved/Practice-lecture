d9b6dede4bc5adb85c6584138616878dec8a6b0f
fall-loved <fallloved24@gmail.com>
2025-12-01 16:29:33 +0300

    finished collections-linq task

---
+++ collections-linq/Задание 2.cs
@@ -0,0 +1,45 @@
+﻿public class Program
+{
+
+    public static Main()
+    {
+        var phoneBook = new PhoneBook();
+
+        phoneBook.AddPhone("Peter Gof", "+375331112233");
+        phoneBook.AddPhone("John Eof", "+375441234567");
+        phoneBook.AddPhone("Will Smith", "+375251234567");
+
+        Console.WriteLine(phoneBook.GetPhoneByName("John Eof"));
+
+        phoneBook.RemovePhone("Will Smith");
+    }
+
+    public class PhoneBook
+    {
+        //     Разработайте класс PhoneBook, который хранит пары "Имя - Телефон" в коллекции Dictionary<string, string>.
+        //     Реализуйте методы для добавления контакта, поиска номера по имени и удаления контакта.
+        //     Соблюдайте принципы SOLID
+        private static Dictionary<string, string> Phone;
+
+        public PhoneBook()
+        {
+            Phone = new Dictionary<string, string>();
+        }
+
+        public void AddPhone(string name, string phone)
+        {
+            Phone.Add(name, phone);
+        }
+
+        public string? GetPhoneByName(string name)
+        {
+            var phone = Phone.GetValueOrDefault(name);
+            return phone;
+        }
+
+        public void RemovePhone(string name)
+        {
+            Phone.Remove(name);
+        }
+    }
+}
